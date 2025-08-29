392c75c4795e8dfdb5cc653939433935d1255299
vtu26028-commits <vtu26028@veltech.edu.in>
2025-08-29 19:31:30 +0530

    Add files via upload

---
+++ Nostra E Commerce/collection.js
@@ -0,0 +1,31 @@
+var sidenav = document.querySelector(".side-navbar");
+if (sidenav) {
+    sidenav.style.left = "-60%";
+    window.showNavbar = function() {
+        sidenav.style.left = "0";
+    }
+    window.closeNavbar = function() {
+        sidenav.style.left = "-60%";
+    }
+}
+
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredValue = event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count=count+1)
+    {
+        var productname=productlist[count].querySelector("p").textContent
+
+        if(productname.toUpperCase().indexOf(enteredValue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+});
